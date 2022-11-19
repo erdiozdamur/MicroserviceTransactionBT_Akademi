518473d16850427cc36f24d8d9d35d31a34f44d9
erdiozdamur <106508478+erdiozdamur@users.noreply.github.com>
2022-11-19 10:34:44 +0300

    initial commit

---
+++ src/main/java/com/bt_akademi/transaction_management/controller/TransactionContraller.java
@@ -0,0 +1,7 @@
+package com.bt_akademi.transaction_management.controller;
+
+public class TransactionContraller {
+
+
+
+}
---
+++ src/test/java/com/bt_akademi/transaction_management/MicroserviceTransactionBtAkademiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt_akademi.transaction_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTransactionBtAkademiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
